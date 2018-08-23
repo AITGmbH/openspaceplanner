a724a8048f9b9449997fdb5575582475a4da419c
Florian Bader <rio99@gmx.de>
2018-08-24 01:04:04 +0200

    Adds ics export (#1)

--- src/app/shared/edit-button/edit-button.directive.ts
+++ src/app/shared/edit-button/edit-button.directive.ts
@@ -1,23 +1,23 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from "@angular/core";
 
 @Directive({
-    selector: '[appEditButton]'
+    selector: "[appEditButton]"
 })
 export class EditButtonDirective {
     @Input() public appHoverButton: number;
 
     constructor(private el: ElementRef) { }
 
-    @HostListener('mouseenter') onMouseEnter() {
-        const button = this.el.nativeElement.querySelector('.edit-button');
-        if (button != null) {
+    @HostListener("mouseenter") onMouseEnter() {
+        const buttons = this.el.nativeElement.querySelectorAll(".edit-button");
+        for (const button of buttons) {
             button.style.opacity = 1;
         }
     }
 
-    @HostListener('mouseleave') onMouseLeave() {
-        const button = this.el.nativeElement.querySelector('.edit-button');
-        if (button != null) {
+    @HostListener("mouseleave") onMouseLeave() {
+        const buttons = this.el.nativeElement.querySelectorAll(".edit-button");
+        for (const button of buttons) {
             button.style.opacity = this.appHoverButton || 0;
         }
     }
