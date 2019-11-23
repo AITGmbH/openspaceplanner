a838a616fd78be82dd4a14a7f7671fcfb87ee3af
Florian Bader <florian.bader@aitgmbh.de>
2019-11-23 16:59:21 +0100

    Adds dotnet paket and fake tools

--- build.sh
+++ build.sh
@@ -1,28 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu
-set -o pipefail
+dotnet tool restore
 
-# liberated from https://stackoverflow.com/a/18443300/433393
-realpath() {
-  OURPWD=$PWD
-  cd "$(dirname "$1")"
-  LINK=$(readlink "$(basename "$1")")
-  while [ "$LINK" ]; do
-    cd "$(dirname "$LINK")"
-    LINK=$(readlink "$(basename "$1")")
-  done
-  REALPATH="$PWD/$(basename "$1")"
-  cd "$OURPWD"
-  echo "$REALPATH"
-}
-
-TOOL_PATH=$(realpath .fake)
-FAKE="$TOOL_PATH"/fake
-
-if ! [ -e "$FAKE" ]
-then
-  dotnet tool install fake-cli --tool-path "$TOOL_PATH"
-fi
-".paket\paket" "restore"
-"$FAKE" "run build.fsx $@"
+dotnet paket restore
+dotnet fake run build.fsx $@
