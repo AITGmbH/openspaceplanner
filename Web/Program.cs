230aed1d2f79868242619c4b785dc40c068b0e64
Florian Bader <florian.bader@aitgmbh.de>
2019-12-16 17:11:58 +0100

    Removes kestrel

--- Web/Program.cs
+++ Web/Program.cs
@@ -13,7 +13,6 @@ namespace openspace
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder
-                        .UseKestrel()
                         .ConfigureAppConfiguration((hostingContext, config) =>
                         {
                             var env = hostingContext.HostingEnvironment;
