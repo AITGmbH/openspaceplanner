6745412b242db81beadec83bfcee1e0f282575f1
Florian Bader <florian.bader@aitgmbh.de>
2019-12-16 13:50:24 +0100

    Fixes migation issue

--- Web/src/polyfills.ts
+++ Web/src/polyfills.ts
@@ -18,18 +18,8 @@
 * BROWSER POLYFILLS
 */
 
-
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following for the Reflect API. */
-import "core-js/es6/reflect";
-
-
-/** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import "core-js/es7/reflect";
-
 
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
@@ -37,13 +27,10 @@ import "core-js/es7/reflect";
  **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
-
-
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
 import "zone.js/dist/zone";  // Included with Angular CLI.
-
 
 
 /***************************************************************************************************
