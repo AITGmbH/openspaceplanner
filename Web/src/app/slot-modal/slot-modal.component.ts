9e34e7f445ac56b6ef8f1fe98ce31e26f3e8ed3b
Florian Bader <florian.bader@aitgmbh.de>
2021-07-29 16:04:22 +0200

    First draft

--- Web/src/app/slot-modal/slot-modal.component.ts
+++ Web/src/app/slot-modal/slot-modal.component.ts
@@ -1,41 +1,42 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Slot } from '../models/slot';
-import { ModalDialogComponent } from '../modal-dialog/modal-dialog.component';
-import { SessionService } from '../session/session.service';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Slot } from "../models/slot";
+import { ModalDialogComponent } from "../modal-dialog/modal-dialog.component";
+import { SessionService } from "../session/session.service";
 
 @Component({
-  selector: 'app-slot-modal',
-  templateUrl: './slot-modal.component.html'
+    selector: "app-slot-modal",
+    templateUrl: "./slot-modal.component.html",
 })
 export class SlotModalComponent {
-  private _item: Slot;
+    private _item: Slot;
 
-  @Output()
-  public close = new EventEmitter();
+    @Output()
+    public close = new EventEmitter();
 
-  @Input()
-  public isShown = false;
+    @Input()
+    public isShown = false;
 
-  @Input()
-  public get item() {
-    return this._item || new Slot();
-  }
+    @Input()
+    public get item() {
+        return this._item || new Slot();
+    }
 
-  public set item(value) {
-    this._item = value;
-  }
+    public set item(value) {
+        if (!value) return;
+        this._item = value;
+    }
 
-  constructor(private sessionService: SessionService) { }
+    constructor(private sessionService: SessionService) {}
 
-  public save() {
-    this.sessionService.updateSlot(this.item);
-  }
+    public save() {
+        this.sessionService.updateSlot(this.item);
+    }
 
-  public delete() {
-    this.sessionService.deleteSlot(this.item.id);
-  }
+    public delete() {
+        this.sessionService.deleteSlot(this.item.id);
+    }
 
-  public onClose() {
-    this.close.next();
-  }
+    public onClose() {
+        this.close.next();
+    }
 }
